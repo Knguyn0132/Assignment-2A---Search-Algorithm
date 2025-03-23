c4f1da58ec76253902b27f2dd61b3fb78dbbd3e9
Nathan Vu <104991276@student.swin.edu.au>
2025-03-23 17:28:38 +1100

    Initialize GBFS.py
    make GBFS inherit from SearchAlgorithm.

---
+++ gbfs.py
@@ -0,0 +1,11 @@
+from search_algorithm import SearchAlgorithm
+
+class GBFS(SearchAlgorithm):
+    """
+    Implements greedy best-first Search.
+    """
+    def __init__(self, graph):
+        super().__init__(graph)
+
+    def search(self, start, goals):
+        pass
